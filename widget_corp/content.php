3411cfb89be3aa168daee2719462a32bfdf1119b
Max Vianna <emaildumax@gmail.com>
2014-10-16 20:42:12 -0300

    Criando página new_subject.php e refatorando navegação

--- widget_corp/content.php
+++ widget_corp/content.php
@@ -1,42 +1,13 @@
 <?php require_once("includes/connection.php"); ?>
 <?php require_once("includes/functions.php"); ?>
-<?php
-	if (isset($_GET['subj'])) {
-		$sel_subject = get_subject_by_id($_GET['subj']);
-		$sel_page = NULL;
-	} elseif (isset($_GET['page'])) {
-		$sel_subject = NULL;
-		$sel_page = get_page_by_id($_GET['page']);
-	} else {
-		$sel_subject = NULL;
-		$sel_page = NULL;
-	}
-?>
+<?php find_selected_page(); ?>
 <?php include("includes/header.php"); ?>
 <table id="structure">
 	<tr>
 		<td id="navigation">
-			<ul class="subjects">
-		<?php
-		$subject_set = get_all_subjects();
-		while ($subject = mysql_fetch_array($subject_set)) {
-			echo "<li";
-			if ($subject["id"] == $sel_subject['id']) { echo " class=\"selected\""; }
-			echo "><a href=\"content.php?subj=" . urlencode($subject["id"]) . 
-				"\">{$subject["menu_name"]}</a></li>";
-			$page_set = get_pages_for_subject($subject["id"]);
-			echo "<ul class=\"pages\">";
-			while ($page = mysql_fetch_array($page_set)) {
-				echo "<li";
-				if ($page["id"] == $sel_page['id']) { echo " class=\"selected\""; }
-				echo "><a href=\"content.php?page=" . urlencode($page["id"]) .
-					"\">{$page["menu_name"]}</a></li>";
-			}
-			echo "</ul>";
-		}
-
-		?>
-			</ul>
+			<?php echo navigation($sel_subject, $sel_page); ?>
+			<br />
+			<a href="new_subject.php">+ Add a new subject</a>
 		</td>
 		<td id="page">
 		<?php if (!is_null($sel_subject)) { // subject selected ?>
--- widget_corp/includes/functions.php
+++ widget_corp/includes/functions.php
@@ -1,5 +1,6 @@
 <?php
 	// This file is the place to store all basic functions
+
 	function confirm_query($result_set) {
 		if (!$result_set) {
 			die("Database query failed: " . mysql_error());
@@ -26,7 +27,7 @@
 		confirm_query($page_set);
 		return $page_set;
 	}
-
+	
 	function get_subject_by_id($subject_id) {
 		global $connection;
 		$query = "SELECT * ";
@@ -59,5 +60,42 @@
 		} else {
 			return NULL;
 		}
+	}
+
+	function find_selected_page() {
+		global $sel_subject;
+		global $sel_page;
+		if (isset($_GET['subj'])) {
+			$sel_subject = get_subject_by_id($_GET['subj']);
+			$sel_page = NULL;
+		} elseif (isset($_GET['page'])) {
+			$sel_subject = NULL;
+			$sel_page = get_page_by_id($_GET['page']);
+		} else {
+			$sel_subject = NULL;
+			$sel_page = NULL;
+		}
+	}
+
+	function navigation($sel_subject, $sel_page) {
+		$output = "<ul class=\"subjects\">";
+		$subject_set = get_all_subjects();
+		while ($subject = mysql_fetch_array($subject_set)) {
+			$output .= "<li";
+			if ($subject["id"] == $sel_subject['id']) { $output .= " class=\"selected\""; }
+			$output .= "><a href=\"content.php?subj=" . urlencode($subject["id"]) . 
+				"\">{$subject["menu_name"]}</a></li>";
+			$page_set = get_pages_for_subject($subject["id"]);
+			$output .= "<ul class=\"pages\">";
+			while ($page = mysql_fetch_array($page_set)) {
+				$output .= "<li";
+				if ($page["id"] == $sel_page['id']) { $output .= " class=\"selected\""; }
+				$output .= "><a href=\"content.php?page=" . urlencode($page["id"]) .
+					"\">{$page["menu_name"]}</a></li>";
+			}
+			$output .= "</ul>";
+		}
+		$output .= "</ul>";
+		return $output;
 	}
 ?>
