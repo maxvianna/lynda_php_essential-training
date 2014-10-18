f082eb8c80512d49d34c7d12f86d1eb449e74748
Max Vianna <emaildumax@gmail.com>
2014-10-18 20:16:16 -0300

    Validando dados do formulário new_subject.php

--- widget_corp/includes/functions.php
+++ widget_corp/includes/functions.php
@@ -16,6 +16,13 @@
 		return $value;
 	}
 
+	function redirect_to( $location = NULL ) {
+		if ($location != NULL) {
+			header("Location: {$location}");
+			exit;
+		}
+	}
+
 	function confirm_query($result_set) {
 		if (!$result_set) {
 			die("Database query failed: " . mysql_error());
